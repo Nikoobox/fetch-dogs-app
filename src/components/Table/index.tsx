8bc18f8d0c68878a2907c88c2eb4edf32d7442ec
Nikolay Shatalov <nikolayshatalov@Nikolays-MacBook-Pro.local>
2024-09-22 11:55:31 -0400

    branch-3: fix infinite scrolling and display data in table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { FC } from "react";
 import {
   Table,
   TableBody,
@@ -10,44 +10,68 @@ import {
   CircularProgress,
 } from "@mui/material";
 
-interface Dog {
-  id: string;
-  breed: string;
-  name: string;
-  age: number;
-}
+import { DogProps } from "../../api";
 
 interface DogTableProps {
-  dogData: Dog[];
+  dogData: DogProps[];
   isLoading: boolean;
+  tableRef?: React.Ref<HTMLDivElement>; // Add ref prop for forwarding
 }
 
-const DogTable: React.FC<DogTableProps> = ({ dogData, isLoading }) => {
+const DogTable: FC<DogTableProps> = ({ dogData, isLoading, tableRef }) => {
   return (
-    <TableContainer
-      component={Paper}
-      //   style={{ maxHeight: "300px", overflowY: "auto" }}
-    >
-      <Table stickyHeader>
-        <TableHead>
-          <TableRow>
-            <TableCell>Breed</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Age</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {dogData.map((dog) => (
-            <TableRow key={dog.id}>
-              <TableCell>{dog.breed}</TableCell>
-              <TableCell>{dog.name}</TableCell>
-              <TableCell>{dog.age}</TableCell>
+    <div style={{ position: "relative" }}>
+      {isLoading && (
+        <CircularProgress
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            zIndex: 2,
+          }}
+        />
+      )}
+      <TableContainer
+        component={Paper}
+        ref={tableRef}
+        sx={{ maxHeight: "350px", overflowY: "auto" }}
+      >
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ fontWeight: "bold" }}>Breed</TableCell>
+              <TableCell sx={{ fontWeight: "bold" }}>Name</TableCell>
+              <TableCell sx={{ fontWeight: "bold" }}>Age</TableCell>
+              <TableCell sx={{ fontWeight: "bold" }}>Zip Code</TableCell>
+              <TableCell sx={{ fontWeight: "bold" }}>Image</TableCell>
             </TableRow>
-          ))}
-          {isLoading && <CircularProgress style={{ display: "block" }} />}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {dogData.map(({ id, breed, name, age, zip_code, img }) => (
+              <TableRow key={id}>
+                <TableCell>{breed}</TableCell>
+                <TableCell>{name}</TableCell>
+                <TableCell>{age}</TableCell>
+                <TableCell>{zip_code}</TableCell>
+                <TableCell>
+                  <img
+                    src={img}
+                    alt="Dog"
+                    style={{
+                      borderRadius: "50%",
+                      width: "56px",
+                      height: "56px",
+                      objectFit: "cover",
+                    }}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
