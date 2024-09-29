5bb1b9b33c30df6f2d1480515990772c4ddd6bc3
Nikolay Shatalov <nikolayshatalov@Nikolays-MacBook-Pro.local>
2024-09-28 20:49:53 -0400

    b-7: add browser router

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -1,5 +1,6 @@
 import { FC, PropsWithChildren } from "react";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { ThemeProvider } from "@mui/material/styles";
@@ -13,7 +14,7 @@ const AppProviders: FC<PropsWithChildren> = ({ children }) => {
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <BrowserRouter>{children}</BrowserRouter>
+        <HashRouter>{children}</HashRouter>
       </ThemeProvider>
     </Provider>
   );
