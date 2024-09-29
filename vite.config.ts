c5f390745691108630fe968ca98a8fbfedfe3221
Nikolay Shatalov <nikolayshatalov@Nikolays-MacBook-Pro.local>
2024-09-28 21:17:43 -0400

    b-7: cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/fetch-dogs-app/",
   plugins: [react()],
-  build: {
-    outDir: "build", // Ensure this matches what you want
-  },
 });
